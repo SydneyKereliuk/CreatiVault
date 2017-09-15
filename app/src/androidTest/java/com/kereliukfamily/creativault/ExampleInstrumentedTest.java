27e4c6b9fbb763beb3e345184645ed940f6bfae5
syke52 <sydney.kereliuk@gmail.com>
2017-09-15 11:37:09 -0400

    fixes to login logic

--- app/src/androidTest/java/com/kereliukfamily/creativault/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/kereliukfamily/creativault/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.kereliukfamily.watchlogs", appContext.getPackageName());
+        assertEquals("com.kereliukfamily.creativault", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/kereliukfamily/creativault/LoginActivity.java
+++ app/src/main/java/com/kereliukfamily/creativault/LoginActivity.java
@@ -15,13 +15,10 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import java.util.List;
 
 /**
  * A login screen that offers login via email/password.
@@ -29,16 +26,11 @@ import java.util.List;
 public class LoginActivity extends AppCompatActivity{
 
     /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "foo@example.com:hello","bar@example.com:world"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -180,30 +172,12 @@ public class LoginActivity extends AppCompatActivity{
         }
     }
 
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        mEmailView.setAdapter(adapter);
-    }
-
-
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
 
     /**
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
      */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+    private class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
         private final String mEmail;
         private final String mPassword;
@@ -224,16 +198,18 @@ public class LoginActivity extends AppCompatActivity{
                 return false;
             }
 
+            boolean isValid = false;
             for (String credential : DUMMY_CREDENTIALS) {
                 String[] pieces = credential.split(":");
                 if (pieces[0].equals(mEmail)) {
                     // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
+                    if(pieces[1].equals(mPassword))
+                    {
+                        isValid=true;
+                    }
                 }
             }
-
-            // TODO: register the new account here.
-            return true;
+            return isValid;
         }
 
         @Override
@@ -245,8 +221,25 @@ public class LoginActivity extends AppCompatActivity{
                 Intent intent = new Intent(getBaseContext(),LandingPage.class);
                 startActivity(intent);
             } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
+                boolean accountExists = false;
+                for (String credential : DUMMY_CREDENTIALS) {
+                    String[] pieces = credential.split(":");
+                    if (pieces[0].equalsIgnoreCase(mEmail)) {
+                        accountExists = true;
+                    }
+                }
+
+                if(accountExists)
+                {
+                    mPasswordView.setError(getString(R.string.error_incorrect_password));
+                    mPasswordView.requestFocus();
+                }
+                else
+                {
+                    mEmailView.setError(getString(R.string.error_invalid_email));
+                    mEmailView.requestFocus();
+                }
+
             }
         }
 
