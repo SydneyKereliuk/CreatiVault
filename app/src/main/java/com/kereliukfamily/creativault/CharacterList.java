47e58694c0e5ed5f5e97438ba2e9022b0dcc9448
Unknown <sydney.kereliuk@gmail.com>
2017-09-28 15:52:56 -0400

    added new activities
    Main menu acctivity, character list, add character

---
+++ app/src/main/java/com/kereliukfamily/creativault/CharacterList.java
@@ -0,0 +1,15 @@
+package com.kereliukfamily.creativault;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kereliukfamily.watchlogs.R;
+
+public class CharacterList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_list);
+    }
+}
